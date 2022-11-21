2f99f148fc74da22688560c95101da294ac584dc
13Dima <sherekhovdima@gmail.com>
2022-11-21 22:10:10 +0200

    JSONfail

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, } from 'react-native';
+import Test from './Test.json'
+
+
 
 export default function App() {
+
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {Test.map((obj) => {
+        let str = []
+        for (const [key, value] of Object.entries(obj)) {
+          str.push(<Text >
+            {key}: {value}
+          </Text>)
+        }
+        return str
+      })}
     </View>
   );
 }
@@ -18,3 +30,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
